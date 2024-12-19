608a85d0e2604819aff0923bcd4af82ceb589b7d
Steven Winters <mstevensoncc2007@gmail.com>
2024-12-19 23:21:51 +0800

    Fixed Scroll

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
   return (
     <>
       <NavBar />
